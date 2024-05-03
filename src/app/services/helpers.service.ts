6765065eeaa62307d5cd4baea0efd2737b7f3f99
Antonio Sanchez <antonio@asanchez.dev>
2024-05-03 11:27:31 +0200

    improve presentMessage function

--- src/app/services/helpers.service.ts
+++ src/app/services/helpers.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
 import { marked } from 'marked';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HelpersService {
 
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer) { }
 
   // presentMessage converts the LLM output to HTML
   presentMessage(message: string) {
-    message = message
-      .replace(/(?:\r\n|\r|\n)/g, '<br>')
-      .replace(/(https?:\/\/[^\s]+)(?![\w/])/g, '<a href="$1" target="_blank">$1</a>');
-
-    return marked.parse(message);
+    // First, replace URLs with anchor tags
+    message = message.replace(/(https?:\/\/[^\s]+)(?![\w/])/g, '<a href="$1" target="_blank">$1</a>');
+    // Then, parse the markdown to HTML
+    let parsedHtml = marked.parse(message).toString();
+    // Finally, replace newline characters with <br> tags
+    parsedHtml = parsedHtml.replace(/(?:\r\n|\r|\n)/g, '<br>');
+    // Use DomSanitizer to bypass Angular's HTML sanitizer
+    return this.sanitizer.bypassSecurityTrustHtml(parsedHtml);
   }
 }
